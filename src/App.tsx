ce9302d23c9371ec5293df6d1cfbe184d868e6d0
Gilbert <ktf2002kzz@gmail.com>
2020-01-21 15:18:32 +0900

    add textCanvas

--- src/App.tsx
+++ src/App.tsx
@@ -44,23 +44,64 @@ const EditorCanvas: React.FC<CanvasProps> = ({ renderedText }) => {
 
   useEffect(()=> {
     Jimp.read(imgSrc, async (err, imgLoaded) => {
-
+      
+      const fontHeight = 50;
+      const bottomX = 10
+      const bottomY = 0;
+      const shadowOffset = 2;
       // get text
       console.log(`[TEXT FROM PROPS]${renderedText}`)
 
+      
+      const textCanvasHeight = 100;
+      const textCanvasWidth = 300;
+      let transparentColor: number = await (Jimp as any).rgbaToInt(100, 100, 0, 0.5);
+      let textCanvas = await new Jimp(textCanvasWidth, textCanvasHeight, transparentColor);
+      // img resize
+      await imgLoaded.resize(1280, 720);
       // load font
-      const fontPath = '';
-      // let font = await Jimp.loadFont(fontPath);
+      // const fontPath = `${process.env.PUBLIC_URL}/nanum/nanum.fnt`;
+      const fontPath = `./nanum/nanum.fnt`;      
+      let font = await Jimp.loadFont(fontPath);
       // print text to the bottom line
 
-      // print shadow of the all text
-      // await imgLoaded.color([{ apply: 'xor', params:['#ffffff']}])
-      // .invert()
-      // .print(font, 2, 2, text)
+      const invertHex = (hex: string) => {
+        hex = hex.slice(1);
+        let hexString = (Number(`0x1${hex}`) ^ 0xFFFFFF).toString(16).substr(1).toUpperCase();
+        return `#${hexString}`
+      }
+      let orangeColor = '#ffd042'
+      let orangeColorHexInverse = invertHex(orangeColor); // Returns FF00FF
+      // let orangeColorHexInverse = invertHex('edc261'); // Returns FF00FF
 
-      // img resize
+      // print shadow of the all text
+      await textCanvas.color([{ apply: 'xor', params:['#000000']}])
+        .invert()
+        .print(font, bottomX + shadowOffset, bottomY + shadowOffset, renderedText)
+        .color([{ apply: 'xor', params:['#ffffff']}])
+
+      // print text
+        .color([{ apply: 'xor', params:[orangeColor]}])
+        .invert()
+        .print(font, bottomX, bottomY, renderedText)
+        .color([{ apply: 'xor', params:[orangeColorHexInverse]}])
+
+        .color([{ apply: 'xor', params:['#00ff00']}])
+        .invert()
+        .print(font, bottomX, bottomY, renderedText.slice(0,2))
+        .color([{ apply: 'xor', params:['#ff00ff']}])
+        
+      
+      // resize text
+
+      await textCanvas.resize(textCanvasWidth*2, textCanvasHeight*2);
+      const compositBottomX = 10;
+      const compositBottomY = imgLoaded.getHeight() - textCanvasHeight
+      await imgLoaded.composite(textCanvas, compositBottomX, compositBottomY);
 
       // get base64 encoded imge to use in the img component
+      // let imgBase64 = await textCanvas.getBase64Async(Jimp.MIME_PNG);
+      // setEditedImgSrc(imgBase64);
       let imgBase64 = await imgLoaded.getBase64Async(Jimp.MIME_PNG);
       setEditedImgSrc(imgBase64);
     })
